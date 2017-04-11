3dc17eab4abac6a1928393be16d18b431b96aced
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-11 02:08:45 +0000

    Added command line argument support

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,21 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 const GITHUB_USER = "kevmor";
 const GITHUB_TOKEN = "31d5efea89ab5e45bbcdea9c2e877ba3be36c5d1";
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {
+     throw err;
+  })
+  .on('response', function (response) {
+    console.log('Downloading image...');
+    console.log('Response Status Code: ', response.statusCode, 'Response Message: ', response.statusMessage, 'Content Type: ', response.headers['content-type']);
+  })
+  .on('end', function() {
+    console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var requestOptions = {
@@ -16,20 +31,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request.get(requestOptions, (error, response, body) => {
-    var contributorModule = JSON.parse(body);
-    contributorModule.forEach((item) => request.get(item['avatar_url']).pipe(fs.createWriteStream(`./${item['login']}_avatar.jpg`)));
+    var contributorObject = JSON.parse(body);
+    contributorObject.forEach((item) => {
+      downloadImageByURL(item.avatar_url, './avatars/' + item.login + '.png');
+    });
   })
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-        console.log('Downloading image...');
-        console.log('Response Status Code: ', response.statusCode, 'Response Message: ', response.statusMessage, 'Content Type: ', response.headers['content-type']);
-       })
-       console.log('Download complete.');
-}
+};
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
