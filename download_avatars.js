7b21065f4cf10eb2a75f14feaff0861b8f40b033
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-11 00:00:40 +0000

    Got requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+const GITHUB_USER = "kevmor";
+const GITHUB_TOKEN = "31d5efea89ab5e45bbcdea9c2e877ba3be36c5d1";
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
   // request.get('')
 }
 
