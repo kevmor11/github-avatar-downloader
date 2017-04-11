0e4b8b11d883884005110edd1943bc1cf1527fb8
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-11 00:30:03 +0000

    Setup the get request

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,8 +8,22 @@ const GITHUB_TOKEN = "31d5efea89ab5e45bbcdea9c2e877ba3be36c5d1";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // request.get('')
+  var requestOptions = {
+    uri: requestURL,
+    headers: {
+      'User-Agent': 'github-downloader/1.0'
+    }
+  };
+  request.get(requestOptions)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+        console.log('Downloading image...');
+        console.log('Response Status Code: ', response.statusCode, 'Response Message: ', response.statusMessage, 'Content Type: ', response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream('./avatar.jpg'));
+       console.log('Download complete.');
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
